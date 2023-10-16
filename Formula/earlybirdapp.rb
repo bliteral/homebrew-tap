3757bf6b7c476123734c8e1a502d942c19ec009f
Robert Bolender <robert@bolender.email>
2023-10-15 20:45:50 -0700

    Appease linter

--- Formula/earlybirdapp.rb
+++ Formula/earlybirdapp.rb
@@ -1,15 +1,15 @@
 class Earlybirdapp < Formula
-    desc "CLI for the earlybirdapp.com waitlist service"
-    homepage "https://earlybirdapp.com"
-    url "https://earlybirdapp.com/earlybirdapp-0.0.1.tar.gz"
-    sha256 "1c3de2f64c5df6f39f72f81e513de201b8b83619a1a21bdf234e467fcf0ad3fc"
-    license "MIT"
+  desc "CLI for the earlybirdapp.com waitlist service"
+  homepage "https://earlybirdapp.com"
+  url "https://earlybirdapp.com/earlybirdapp-0.0.1.tar.gz"
+  sha256 "1c3de2f64c5df6f39f72f81e513de201b8b83619a1a21bdf234e467fcf0ad3fc"
+  license "MIT"
 
-    def install
-        bin.install "earlybirdapp"
-    end
+  def install
+    bin.install "earlybirdapp"
+  end
 
-    test do
-        system "true"
-    end
+  test do
+    system "true"
+  end
 end
